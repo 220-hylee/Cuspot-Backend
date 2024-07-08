167f70b0952c375d01d4312e900bcd1ada3409ef
hyenminkim <hyenmin8922@gmail.com>
2024-07-08 15:45:04 +0900

    스프링 부트 올리기

---
+++ src/main/java/com/web/CuspotApplication.java
@@ -0,0 +1,13 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuspotApplication.class, args);
+	}
+
+}
